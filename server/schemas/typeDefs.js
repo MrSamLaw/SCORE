bd60a6c283f6f981cedcd43a26edcae0ebf857a8
Mr Sam Law <sam@chongland.com>
2021-08-02 19:25:19 +1000

    Start JWT Logins

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,12 +3,43 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
 type User {
-    _id:ID
-    name:String!
+    _id: ID
+    name: String!
+    email: String!
+    password: String!
+    
 }
 
+type Auth {
+    token:ID!
+    user:User
+}
+
+type Competitor {
+    _id:ID
+    firstName: String!
+    lastName: String!
+    carNo: Int
+    class(type:ClassType):String
+}
+
+type Qualify {
+    qualOne: Int!
+    qualTwo: Int!
+    competitor: Competitor!
+}
+
+enum ClassType {
+    Pro
+    ProAm
+}
 type Query {
     users:[User]!
+    competitors:[Competitor]!
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
 }
 `;
 
