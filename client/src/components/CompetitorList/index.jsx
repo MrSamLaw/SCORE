3024863e4275285ceeebd3a345b8a9768102cf1e
Mr Sam Law <sam@chongland.com>
2021-08-03 19:25:26 +1000

    Login/Logout Toggle, CompetitorList query & basic styling

--- client/src/components/CompetitorList/index.jsx
+++ client/src/components/CompetitorList/index.jsx
@@ -1,3 +1,5 @@
+import "./competitorlist.scss";
+
 const CompetitorList = ({ competitors, firstName, lastName, carNo }) => {
   if (!competitors.length) {
     return <h3>No Registered Competitors Yet</h3>;
@@ -7,7 +9,15 @@ const CompetitorList = ({ competitors, firstName, lastName, carNo }) => {
     <div>
       {competitors &&
         competitors.map((competitor) => (
-          <div key={competitor._id} className="card"></div>
+          <div key={competitor._id} className="card">
+            <div className="left">
+              <p className="carNo">{competitor.carNo}</p>
+            </div>
+            <div className="right">
+              <p>{competitor.firstName}</p>
+              <p>{competitor.lastName}</p>
+            </div>
+          </div>
         ))}
     </div>
   );
--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import "./nav.scss";
 
+import Auth from "../../utils/auth";
+
 export default function Nav() {
+  const logout = (e) => {
+    e.preventDefault();
+    Auth.logout();
+  };
   return (
     <section className="navbar">
       <div className="wrapper">
@@ -15,14 +21,18 @@ export default function Nav() {
           </ul>
         </div>
         <div className="right">
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
+          {Auth.loggedIn() ? (
+            <button onClick={logout}>Logout</button>
+          ) : (
+            <ul>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </section>
